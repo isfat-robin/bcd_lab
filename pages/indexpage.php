ba33ee87a52e606bb7c117d0f0feed2d03ec0d0c
Isfat Robin <40149750+isfat-robin@users.noreply.github.com>
2020-05-21 14:23:28 +0600

    Update indexpage.php

--- pages/indexpage.php
+++ pages/indexpage.php
@@ -6,28 +6,11 @@
 <head>
 	<title>indpage</title>
 	<style type="text/css">
-		*{
-			margin: 0;
-			padding: 0;
-		}
-		body{
-			background: black;
-			text-align: center;
-			margin: 0 auto;
-		}
-
-		.content{
-			margin: auto;
-		}
-
-		.content>p{
-			text-align: justify;
-			margin: 20px;
-		}
-
-		r{
-			color: red;
-		}
+		*{margin: 0;padding: 0;}
+		body{background: black;text-align: center;margin: 0 auto;}
+		.content{margin: auto;}
+		.content>p{text-align: justify;margin: 20px;}
+		r{color: red;}
 
 	</style>
 </head>
@@ -36,7 +19,7 @@
 		 <img class="mod-logo" src="img/logo.png" alt="">
 		<h2>Welcome to Our SQLi lab</h2>
 		<p>Hey. It's a great news for us, You are using our Lab.
-		Thank's for choosing our lab to learn sql injection. It's a great time to learn Web Security for you. SQL injection is a first step to learn for web penatration testing. So with sql injection every hacker's known about web most common and basic problem's and vulnerability. I suggest that to everyone, please fisrt learn about php and database system and after try to do this. If you start trying to do now, Please At first read about us and Disclaimers (Click About button). <br>
+		Thank's for choosing our lab to learn sql injection. It's a great time to learn Web Security for you. SQL injection is a first step to learn for web penatration testing. So with sql injection every hacker's known about web most common and basic problem's and vulnerability. I suggest that to everyone, please fisrt learn about php and database system and after try to do this. And make  sure php v5.6 is running on server for avoiding unexpected errors. If you start trying to do now, Please At first read about us and Disclaimers (Click About button). <br>
 		 </p><br>
 		 <h3> <r>Your injection result is <?php echo $job_done; ?> </r> <br>Best Of luck. :) </h3>
 		
